5f8bedcd0b6f10651db2dab9312c2937105a2af9
Doruk Sülün <doruk_phasianum@icloud.com>
2026-01-15 12:31:04 +0300

    feat: add ideal weight calculator

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ gender = input("\n\t\tEnter your gender(m/f): ").lower()
 #height cm -> meter
 height_m = height_cm/(10**2)
 
+#height cm -> inch
+height_inch = height_cm * 0.393
+
 #calculate BMI
 bmi = weight/(height_m**2)
 
@@ -16,12 +19,15 @@ water_requirenment = weight * 0.035
 
 if gender == 'm':
     bmr = 88.362 + (13.397 * weight) + (4.799 * height_cm) - (5.677 * age)
+    ibw = 50 + 2.3 * (height_inch-60)                                           #calculate to IBW(Ideal body weight)
 else:
     bmr = 447.593 + (9.247 * weight) + (3.098 * height_cm) - (4.330 * age)
+    ibw = 45.5 + 2.3 * (height_inch - 60)
 
 print("\n" + "-" * 60)
 print("\n")
 print(f"\t\tYour BMI: {round(bmi,2)}")
 print(f"\n\n\t\tYour BMR: {round(bmr,2)}")
 print(f"\n\n\t\tYour daily water requirenment: {round(water_requirenment,2)}")
+print(f"\n\n\t\tYour IBW(ideal body weight): {round(ibw,2)}")
 print("\n" + "-" * 60)
