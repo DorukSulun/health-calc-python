a4a52f89e3b31ef51b1fed688231458e29fe6cb9
Doruk Sülün <doruk_phasianum@icloud.com>
2026-01-15 12:13:01 +0300

    feat: add BMR calculation using Harris-Benedict equation

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 print("\t---Welcome to Personal Health Calculator---") 
 
-weight = float(input("\n\tEnter your weight(kg): "))
-height_cm = float(input("\n\tEnter your height(cm): "))
-age = int(input("\n\tEnter your age: "))
-gender = input("\n\tEnter your gender(m/f): ").lower()
+weight = float(input("\n\t\tEnter your weight(kg): "))
+height_cm = float(input("\n\t\tEnter your height(cm): "))
+age = int(input("\n\t\tEnter your age: "))
+gender = input("\n\t\tEnter your gender(m/f): ").lower()
 
 #height cm -> meter
 height_m = height_cm/(10**2)
 
 #calculate BMI
 bmi = weight/(height_m**2)
+
+if gender == 'm':
+    bmr = 88.362 + (13.397 * weight) + (4.799 * height_cm) - (5.677 * age)
+else:
+    bmr = 447.593 + (9.247 * weight) + (3.098 * height_cm) - (4.330 * age)
